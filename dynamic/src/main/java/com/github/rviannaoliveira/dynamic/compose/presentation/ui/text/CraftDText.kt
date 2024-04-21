3bf2fe47b3dabe610a8dc0e6cb6c0f63c507137f
Rodrigo Vianna <rodrigo.vianna.oliveira@gmail.com>
2024-04-21 07:53:00 -0300

    create app-sample without rviannaoliveira

--- dynamic/src/main/java/com/github/rviannaoliveira/dynamic/compose/presentation/ui/text/CraftDText.kt
+++ dynamic/src/main/java/com/github/rviannaoliveira/dynamic/compose/presentation/ui/text/CraftDText.kt
@@ -17,7 +17,7 @@ import com.github.rviannaoliveira.dynamic.compose.presentation.ui.toTextStyle
 import com.github.rviannaoliveira.dynamic.xml.presentation.ui.button.parseColorCompose
 
 @Composable
-fun DynamicText(
+fun CraftDText(
     textProperties: TextProperties,
     modifier: Modifier = Modifier,
     clickable: (() -> Unit)? = null,
