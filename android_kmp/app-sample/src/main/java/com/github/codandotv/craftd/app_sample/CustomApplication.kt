d364af9760a45c49134816d58ba9520c1ef17ad5
Rods <rodrigo.vianna.oliveira@gmail.com>
2024-04-29 20:19:45 -0300

    fix conflict

---
+++ android_kmp/app-sample/src/main/java/com/github/codandotv/craftd/app_sample/CustomApplication.kt
@@ -0,0 +1,19 @@
+package com.github.codandotv.craftd.app_sample
+
+import android.app.Application
+import com.github.codandotv.craftd.app_sample.di.AppModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.EmptyLogger
+
+class CustomApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            if (BuildConfig.DEBUG) androidLogger(org.koin.core.logger.Level.ERROR) else EmptyLogger()
+            androidContext(this@CustomApplication)
+            modules(AppModule.instance)
+        }
+    }
+}
