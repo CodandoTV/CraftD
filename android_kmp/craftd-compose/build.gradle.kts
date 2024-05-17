9fe99cd23b30997b0cbc8f1fdaa10c6656e4c248
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2024-05-17 10:55:07 -0300

    Generate compose metrics

--- android_kmp/craftd-compose/build.gradle.kts
+++ android_kmp/craftd-compose/build.gradle.kts
@@ -10,3 +10,26 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.google.material)
 }
+
+/**
+ * Compose metrics
+ *
+ * You can generate the compose metrics report using the following task:
+ *
+ * To be able to see a beutiful report, the community recommends to use [Mendable](https://github.com/jayasuryat/mendable).
+ *
+ * You can check the metrics in the directory <this module>/build/compose_metrics.
+ *
+ */
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().all {
+    kotlinOptions.freeCompilerArgs += listOf(
+        "-P",
+        "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=" +
+                project.buildDir.absolutePath + "/compose_metrics"
+    )
+    kotlinOptions.freeCompilerArgs += listOf(
+        "-P",
+        "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=" +
+                project.buildDir.absolutePath + "/compose_metrics"
+    )
+}
