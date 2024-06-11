1fcd220e576ad00dc7ca392da06536ea4e65b661
Gabriel Bronzatti Moro <gabrielbronzattimoro.es@gmail.com>
2024-06-11 08:49:23 -0300

    Move all android code to be part of android source set

--- android_kmp/craftd-core/build.gradle.kts
+++ android_kmp/craftd-core/build.gradle.kts
@@ -3,11 +3,15 @@ plugins {
     id("com.codandotv.publish")
 }
 
-dependencies {
-    implementation(libs.compose.lifecycle)
-    implementation(libs.androidx.core)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.google.material)
-    implementation(libs.fasterxml.jackson)
-    implementation(libs.fasterxml.jackson.databind)
+kotlin {
+    sourceSets {
+        androidMain.dependencies {
+            implementation(libs.compose.lifecycle)
+            implementation(libs.androidx.core)
+            implementation(libs.androidx.appcompat)
+            implementation(libs.google.material)
+            implementation(libs.fasterxml.jackson)
+            implementation(libs.fasterxml.jackson.databind)
+        }
+    }
 }
