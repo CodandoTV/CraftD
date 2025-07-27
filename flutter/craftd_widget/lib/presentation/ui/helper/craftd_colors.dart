1e2cad1513d4435ddd825a11e5b89832f91da5b9
Gabriel Moro <gabrielbronzattimoro.es@gmail.com>
2025-07-27 17:57:44 -0300

    Setup flutter linter only for the craftD library

--- flutter/craftd_widget/lib/presentation/ui/helper/craftd_colors.dart
+++ flutter/craftd_widget/lib/presentation/ui/helper/craftd_colors.dart
@@ -5,7 +5,7 @@ class CraftDColor{
     try{
       hexColor = hexColor!.replaceAll('#', '');
       if (hexColor.length == 6) {
-        hexColor = 'FF' + hexColor;
+        hexColor = 'FF$hexColor';
       }
       return Color(int.parse(hexColor, radix: 16));
     } catch (e) {
