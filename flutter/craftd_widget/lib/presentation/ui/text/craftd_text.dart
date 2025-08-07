467de2bdc78f8783dce5f84a720827e3135e68d4
Gabriel Moro <gabrielbronzattimoro.es@gmail.com>
2025-08-07 09:31:11 -0300

    Refactor: Add `fillMaxSize` and `textAllCaps` properties to CraftDButton
    This commit introduces two new properties to the `CraftDButton` widget:
    
    - `fillMaxSize`: When set to `true`, the button will expand to fill the available width.
    - `textAllCaps`: When set to `true`, the button's text will be displayed in uppercase.
    
    Corresponding snapshot tests have been added to verify these new functionalities.
    The `CraftDText` widget was also updated to respect the `textAllCaps` property.

--- flutter/craftd_widget/lib/presentation/ui/text/craftd_text.dart
+++ flutter/craftd_widget/lib/presentation/ui/text/craftd_text.dart
@@ -7,19 +7,24 @@ class CraftDText extends StatelessWidget {
   final TextProperties textProperties;
   final VoidCallback? callback;
 
-  const CraftDText(
-      {super.key, required this.textProperties, this.callback});
+  const CraftDText({super.key, required this.textProperties, this.callback});
 
   @override
   Widget build(BuildContext context) {
+    final String text = textProperties.textAllCaps == true
+        ? textProperties.text.toUpperCase()
+        : textProperties.text;
     return GestureDetector(
-        onTap: callback,
-        child: Text(textProperties.text,
-            style: TextStyle(
-              fontSize: textProperties.textSize != null
-                  ? double.tryParse(textProperties.textSize!)
-                  : 16.0,
-              color: CraftDColor.hexToColor(textProperties.textColorHex),
-            )));
+      onTap: callback,
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: textProperties.textSize != null
+              ? double.tryParse(textProperties.textSize!)
+              : 16.0,
+          color: CraftDColor.hexToColor(textProperties.textColorHex),
+        ),
+      ),
+    );
   }
 }
--- flutter/craftd_widget/test/snapshot/craftd_button_snapshot_test.dart
+++ flutter/craftd_widget/test/snapshot/craftd_button_snapshot_test.dart
@@ -6,13 +6,13 @@ import '../util/widget_util.dart';
 
 void main() {
   testWidgets(
-    'CraftD - Button',
+    'CraftD - Button with allcaps true',
     (tester) async {
       const buttonProperties = ButtonProperties(
         text: 'Tap',
         textColorHex: '#800080',
         backgroundHex: '#000000',
-        textSize: '24.0',
+        textSize: '32.0',
         textAllCaps: true,
       );
       final widget = await WidgetsUtil.buildMaterialAppWidgetTest(
@@ -27,7 +27,34 @@ void main() {
 
       await expectLater(
         find.byType(CraftDButton),
-        matchesGoldenFile('goldens/craftd_button_snapshot.png'),
+        matchesGoldenFile('goldens/craftd_button_allcaps_snapshot.png'),
+      );
+    },
+  );
+
+  testWidgets(
+    'CraftD - Button with a fillMaxSize true',
+        (tester) async {
+      const buttonProperties = ButtonProperties(
+        text: 'Tap',
+        textColorHex: '#000000',
+        backgroundHex: '#800080',
+        textSize: '32.0',
+        fillMaxSize: true,
+      );
+      final widget = await WidgetsUtil.buildMaterialAppWidgetTest(
+        child: CraftDButton(
+          buttonProperties: buttonProperties,
+          callback: () => {},
+        ),
+        tester: tester,
+      );
+
+      await tester.pumpWidget(widget);
+
+      await expectLater(
+        find.byType(CraftDButton),
+        matchesGoldenFile('goldens/craftd_button_fill_max_size_snapshot.png'),
       );
     },
   );
