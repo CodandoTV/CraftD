b64b79ae854806c477dc0298f85e22b317af12cf
Rods <rodrigo.vianna.oliveira@gmail.com>
2025-11-13 09:25:53 -0300

    [issue-59] - works

--- android_kmp/KMP-Sample/composeApp/build.gradle.kts
+++ android_kmp/KMP-Sample/composeApp/build.gradle.kts
@@ -31,8 +31,8 @@ kotlin {
             implementation(libs.androidx.activity.compose)
         }
         commonMain.dependencies {
-//            implementation(libs.craftd.compose)
-            implementation(libs.craftd.core)
+            implementation(libs.craftd.compose)
+//            implementation(libs.craftd.core)
             implementation(compose.runtime)
             implementation(libs.kotlinx.coroutines.core)
             implementation(libs.kotlinx.serialization.json)
